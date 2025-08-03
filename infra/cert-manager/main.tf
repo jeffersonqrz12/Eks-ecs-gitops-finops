215c52c5646b34ed6420241352ec0cf69362651a
jeffersonqrz12 <jeffersonqrz1@gmail.com>
2025-08-03 17:18:24 -0300

    Create main.tf

---
+++ infra/cert-manager/main.tf
@@ -0,0 +1,20 @@
+provider "helm" {
+  kubernetes {
+    config_path = "~/.kube/config"
+  }
+}
+
+resource "helm_release" "cert_manager" {
+  name       = "cert-manager"
+  namespace  = "cert-manager"
+  chart      = "cert-manager"
+  repository = "https://charts.jetstack.io"
+  version    = "v1.14.3"
+
+  create_namespace = true
+
+  set {
+    name  = "installCRDs"
+    value = "true"
+  }
+}
