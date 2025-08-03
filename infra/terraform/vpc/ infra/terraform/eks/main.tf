735599d72937b44c9e9612aa377dc1d5636b64fd
jeffersonqrz12 <jeffersonqrz1@gmail.com>
2025-08-03 11:07:26 -0300

    Create main.tf

---
+++ infra/terraform/vpc/ infra/terraform/eks/main.tf
@@ -0,0 +1,7 @@
+module "eks" {
+  source          = "terraform-aws-modules/eks/aws"
+  cluster_name    = var.cluster_name
+  cluster_version = "1.29"
+  subnets         = var.subnets
+  vpc_id          = var.vpc_id
+}
